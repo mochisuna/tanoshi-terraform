9163608a40e84759941e6c479b715c29b852a574
mochisuna <hirokazu.maruta@gmail.com>
2019-02-12 14:58:09 +0900

    update 微調整

--- modules/aws/compute/app/output.tf
+++ modules/aws/compute/app/output.tf
@@ -5,6 +5,7 @@ output "outputs" {
       "scale_in_policy_arn", "${aws_autoscaling_policy.scale_in_cpu_policy.arn}",
       "scale_out_policy_name", "${aws_autoscaling_policy.scale_out_cpu_policy.name}",
       "scale_out_policy_arn", "${aws_autoscaling_policy.scale_out_cpu_policy.arn}",
+      "alb_arn", "${aws_lb.alb.arn}"
     )
   }"
 }
--- modules/aws/vpc/public.tf
+++ modules/aws/vpc/public.tf
@@ -1,6 +1,8 @@
 # vpc setting
 resource "aws_vpc" "vpc" {
-  cidr_block = "${lookup(var.vpc_cider_blocks, "${terraform.env}.cidr", var.vpc_cider_blocks["default.cidr"])}"
+  cidr_block           = "${lookup(var.vpc_cider_blocks, "${terraform.env}.cidr", var.vpc_cider_blocks["default.cidr"])}"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
 
   tags {
     Name = "${var.vpc["name"]}"
